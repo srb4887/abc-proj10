f971ce5838458665e7a3e737a36e1bfd6f0b5084
srb4887 <srb4887@gmail.com>
2017-08-08 21:28:30 -0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,126 @@
+<template>
+  <div id="app" class="container">
+    <div class="page-header">
+      <h1>Vue.js 2 & Firebase Sample Application</h1>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3>Add Book</h3>
+      </div>
+      <div class="panel-body">
+        <form id="form" class="form-inline" v-on:submit.prevent="addBook">
+          <div class="form-group">
+            <label for="bookTitle">Title:</label>
+            <input type="text" id="bookTitle" class="form-control" v-model="newBook.title">
+          </div>
+          <div class="form-group">
+            <label for="bookAuthor">Author:</label>
+            <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author">
+          </div>
+          <div class="form-group">
+            <label for="bookUrl">URL:</label>
+            <input type="text" id="bookUrl" class="form-control" v-model="newBook.url">
+          </div>
+          <input type="submit" class="btn btn-primary" value="Add Book">
+        </form>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3>Books Lists</h3>
+      </div>
+      <div class="panel-body">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>
+                Title
+              </th>
+              <th>
+                Author
+              </th>
+              <th>
+                Delete
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="book in books">
+              <td>
+                <a v-bind:href="book.url">{{book.title}}</a>
+              </td>
+              <td>
+                {{book.author}}
+              </td>
+              <td>
+                <span class="glyphicon glyphicon-trash" v-on:click="removeBook(book)"></span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+import Firebase from 'firebase'
+
+import toastr from 'toastr'
+
+let config = {
+  apiKey: "AIzaSyAKD3vnKnnBPo7sGQk9RXsMuUYsUZ4qkMU",
+  authDomain: "vuejs-firebase-01-3deb6.firebaseapp.com",
+  databaseURL: "https://vuejs-firebase-01-3deb6.firebaseio.com",
+  projectId: "vuejs-firebase-01-3deb6",
+  storageBucket: "vuejs-firebase-01-3deb6.appspot.com",
+  messagingSenderId: "610356450163"
+}
+
+let app = Firebase.initializeApp(config);
+let db = app.database();
+
+let booksRef = db.ref('books');
+
+export default {
+  name: 'app',
+  firebase: {
+    books: booksRef
+  },
+  data () {
+    return {
+      newBook: {
+        title: '',
+        author: '',
+        url: ''
+      }
+    }
+  },
+  methods: {
+    addBook: function(){
+      booksRef.push(this.newBook);
+      this.newBook.title = '';
+      this.newBook.author = '';
+      this.newBook.url = '';
+    },
+    removeBook: function(book){
+      booksRef.child(book['.key']).remove();
+      toastr.success("Book removed");
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
